44a8414fb562a29aeea52df735a70cb21b7fef79
Вадим Кончик <KonchikV@eurotorg.by>
2022-08-24 10:33:40 +0300

    Added business route

--- src/controllers/business/index.ts
+++ src/controllers/business/index.ts
@@ -21,22 +21,33 @@ router.get("/list", async (req, res) => {
 
 router.get("/one/:id", async (req, res) => {
   try {
-    const [main, services] = await Promise.all([
-      database("business_card_main"),
-      database("business_card_service").where({
+    const [main = [], services = []] = await Promise.all([
+      database("business_card_main").where({
         id: req.params.id,
+      }),
+      database("business_card_service").where({
+        business_id: req.params.id,
       }),
     ]);
 
-    const business = main[0];
-    business.services = services;
+    if (main.length) {
+      const business = main[0];
 
-    res.status(200).json(business);
+      business.services = services;
+
+      res.status(200).json(business);
+    } else {
+      res
+        .status(404)
+        .json({ error: true, code: 404, message: "Запись не найдена" });
+    }
   } catch (err) {
-    console.log(err);
+    console.error(err);
 
-    res.status(200).json({
-      business: [],
+    res.status(500).json({
+      error: true,
+      code: 500,
+      message: "Произошла техническая ошибка",
     });
   }
 });
