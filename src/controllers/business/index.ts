7a5588c05901d95dad84f3ba3a5ed4017bbc4cad
Alex Tesemnikov <Tesemnikov@eurotorg.by>
2022-08-26 16:46:52 +0300

    feat: add info

--- src/controllers/business/index.ts
+++ src/controllers/business/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { database } from "../../db"
+import { getInfoForCardBusiness } from "../../utils/getInfoForCardBusiness"
 import { getListForCardBusiness } from "../../utils/getListForCardBusiness"
 import { getTableForCardBusiness } from "../../utils/getTableForCardBusiness"
 
@@ -23,8 +24,8 @@ router.get("/list", async (req, res) => {
 
 router.get("/one/:id", async (req, res) => {
   try {
-    const [main = [], services = [], list = [], table = []] = await Promise.all(
-      [
+    const [main = [], services = [], list = [], table = [], info = []] =
+      await Promise.all([
         database("business_card_main").where({
           id: req.params.id,
         }),
@@ -37,16 +38,21 @@ router.get("/one/:id", async (req, res) => {
         database("business_card_service_table").where({
           service_id: req.params.id,
         }),
-      ]
-    )
+        database("business_card_info").where({
+          business_id: req.params.id,
+        }),
+      ])
 
     if (main.length) {
       const serviceList = await getListForCardBusiness(list)
       const serviceTable = await getTableForCardBusiness(table)
+      const information = await getInfoForCardBusiness(info)
+
       const business = main[0]
       business.services = services[0]
       business.services.serviceList = serviceList
       business.services.serviceTable = serviceTable
+      business.information = information
 
       res.status(200).json(business)
     } else {
---
+++ src/utils/getInfoForCardBusiness.ts
@@ -0,0 +1,58 @@
+import { database } from "../db"
+
+// interface IInfo {
+//   id: number
+// }
+
+export const getInfoForCardBusiness = async (info: any): Promise<any> => {
+  const sch: { id: number; schedule: { day: string; time: string }[] }[] | any =
+    []
+  for (let i = 0; i < info.length; i += 1) {
+    try {
+      const [
+        descr = [],
+        contacts = [],
+        schedule = [],
+        address = [],
+        social = [],
+      ] = await Promise.all([
+        database("business_card_info_descr").where({
+          info_id: info[i].id,
+        }),
+        database("business_card_info_contacts_content").where({
+          info_contacts_id: info[i].id,
+        }),
+        database("business_card_info_schedule").where({
+          info_id: info[i].id,
+        }),
+        database("business_card_info_address").where({
+          info_id: info[i].id,
+        }),
+        database("business_card_info_social").where({
+          info_id: info[i].id,
+        }),
+      ])
+      sch.push({
+        id: info[i].id,
+        descr: descr,
+        contacts: contacts,
+        schedule: schedule,
+        address: address,
+        social: social,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const result = info.map((item: any) => {
+    return {
+      ...item,
+      descr: sch[item.id - 1].descr,
+      contacts: sch[item.id - 1].contacts,
+      schedule: sch[item.id - 1].schedule,
+      address: sch[item.id - 1].address,
+      social: sch[item.id - 1].social,
+    }
+  })
+  return result
+}
