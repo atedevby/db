10958f9b058c38aeadee6358c8ce0997ec75e7c5
Alex Tesemnikov <atedevby@gmail.com>
2022-09-13 07:38:34 +0300

    test

--- src/controllers/news/index.ts
+++ src/controllers/news/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {database} from "../../db";
+import {database,daksDB} from "../../db";
 
 const router = express.Router();
 
@@ -18,5 +18,60 @@ router.get("/", async (req, res) => {
         });
     }
 });
+
+router.post("/news/:id", async (req, res) => {
+    try {
+      const news: any = await daksDB("news").insert({
+        enabled: req.body.enabled,
+        title: req.body.title,
+        descr: req.body.descr,
+        url_for_banner: req.body.url_for_banner,
+        city: req.body.city,
+        author: req.body.author,
+        createAt: req.body.createAt
+      })
+    //   const id = req.body.mainId
+    //   const [services = [], list = []] =
+    //     // table = [], info = [], gallery = []
+    //     await Promise.all([
+    //       database("business_card_service").insert({
+    //         business_id: id,
+    //         name: req.body.main.services.name,
+    //       }),
+    //       database("business_card_service_list").where({
+    //         service_id: id,
+    //         enabled: req.body.main.services.list.enabled,
+    //         title: req.body.main.services.list.title,
+    //         price: req.body.main.services.list.price,
+    //         descr: req.body.main.services.list.descr,
+    //         name: req.body.main.services.list.name,
+    //       }),
+    //       // database("business_card_service_table").where({
+    //       //   service_id: id,
+    //       // }),
+    //       // database("business_card_info").where({
+    //       //   business_id: id,
+    //       // }),
+    //       // database("business_card_gallery_list").where({
+    //       //   business_card_gallery_id: id,
+    //       // }),
+    //     ])
+    //   business.services = services
+    //   business.servicesList = list
+  
+      res.status(200).json({
+        success: true,
+        message: "Карточка добавлена",
+        news
+      })
+    } catch (err) {
+      console.error(err)
+  
+      res.status(500).json({
+        error: true,
+        message: "Внутренняя ошибка",
+      })
+    }
+  })
 
 export const news = router;
--- src/db/index.ts
+++ src/db/index.ts
@@ -14,3 +14,18 @@ export const database = knex({
         max: 10,
     },
 });
+
+export const daksDB = knex({
+    client: 'mysql2',
+    connection: {
+        host: '93.85.88.56',
+        user: 'aksen',
+        password: 'aksen_Adw123dlDa_~asZ',
+        database: 'daks_db',
+        charset: 'utf8',
+    },
+    pool: {
+        min: 0,
+        max: 10,
+    },
+});
