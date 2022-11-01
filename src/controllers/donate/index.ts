23360d7a6e2923aea804b55f7700bea0b4d76e65
Alex Tesemnikov <Tesemnikov@eurotorg.by>
2022-11-01 19:53:20 +0300

    refact: change getDonate

--- src/controllers/donate/index.ts
+++ src/controllers/donate/index.ts
@@ -16,13 +16,53 @@ export const getDonate = async (req: any, res: any) => {
   }
 }
 
-export const updateDonateInformation = async (req: any, res: any) => {
+export const updateDonateDescription = async (req: any, res: any) => {
   try {
-    const { banner, description, sum } = req.body
+    const { description } = req.body
+    const id = req.params.id
+    const donate = await daksDB("donate").update({
+      description: description,
+      id: id,
+    })
+
+    res.status(200).json({
+      donate,
+    })
+  } catch (err) {
+    console.log(err)
+
+    res.status(200).json({
+      donate: [],
+    })
+  }
+}
+
+export const updateDonateBanner = async (req: any, res: any) => {
+  try {
+    const { banner } = req.body
     const id = req.params.id
     const donate = await daksDB("donate").update({
       banner: banner,
-      description: description,
+      id: id,
+    })
+
+    res.status(200).json({
+      donate,
+    })
+  } catch (err) {
+    console.log(err)
+
+    res.status(200).json({
+      donate: [],
+    })
+  }
+}
+
+export const updateDonateSum = async (req: any, res: any) => {
+  try {
+    const { sum } = req.body
+    const id = req.params.id
+    const donate = await daksDB("donate").update({
       sum: sum,
       id: id,
     })
--- src/routes/donate/index.ts
+++ src/routes/donate/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express"
-import { getDonate, updateDonateInformation } from "../../controllers/donate"
+import {
+  getDonate,
+  updateDonateSum,
+  updateDonateDescription,
+  updateDonateBanner,
+} from "../../controllers/donate"
 const router = Router()
 
 router.get("/getDonate", getDonate)
@@ -7,7 +12,9 @@ router.get("/getDonate", getDonate)
 // router.get("/card/commercial/:id", getCurrentCard)
 
 // router.post("/post", updateDonateInformation)
-router.put("/update/:id", updateDonateInformation)
+router.put("/updateSum/:id", updateDonateSum)
+router.put("/updateDescription/:id", updateDonateDescription)
+router.put("/updateBanner/:id", updateDonateBanner)
 
 // router.delete("/api/server/:id", remove)
 
