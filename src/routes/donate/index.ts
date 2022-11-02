28838caad080cb91e7fda5e682ea2c99b8d17fb9
Alex Tesemnikov <Tesemnikov@eurotorg.by>
2022-11-02 15:03:26 +0300

    feat: add update for donate

--- src/routes/donate/index.ts
+++ src/routes/donate/index.ts
@@ -12,7 +12,7 @@ router.get("/getDonate", getDonate)
 // router.get("/card/commercial/:id", getCurrentCard)
 
 // router.post("/post", updateDonateInformation)
-router.put("/updateSum", updateDonateSum)
+router.put("/updateSum/:id", updateDonateSum)
 router.put("/updateDescription/:id", updateDonateDescription)
 router.put("/updateBanner/:id", updateDonateBanner)
 
